822bfdcbe14e5a924c7046c9fd5b20da8919e913
Felix Becker <felix.b@outlook.com>
2020-09-04 11:42:14 +0200

    WIP

--- src/content.ts
+++ src/content.ts
@@ -3,10 +3,58 @@ import './polyfill'
 import { documentToSVG, inlineResources, formatXML } from 'dom-to-svg'
 import { saveAs } from 'file-saver'
 
+const svgNamespace = 'http://www.w3.org/2000/svg'
+
 async function main(): Promise<void> {
 	console.log('Content script running')
 
-	const svgDocument = documentToSVG(document)
+	const { clientWidth, clientHeight } = document.documentElement
+
+	const svgElement = document.createElement('svg')
+	svgElement.setAttribute('viewPort', `0 0 ${clientWidth} ${clientHeight}`)
+	svgElement.style.position = 'fixed'
+	svgElement.style.top = '0px'
+	svgElement.style.left = '0px'
+	svgElement.style.width = `${clientWidth}px`
+	svgElement.style.height = `${clientHeight}px`
+
+	const backdrop = document.createElementNS(svgNamespace, 'rect')
+	backdrop.setAttribute('fill', 'rgba(0, 0, 0, 0.2)')
+	svgElement.append(backdrop)
+
+	const mask = document.createElementNS(svgNamespace, 'mask')
+	mask.id = 'svg-screenshot-cutout'
+
+	const maskBackground = document.createElementNS(svgNamespace, 'rect')
+	maskBackground.setAttribute('fill', 'black')
+	maskBackground.setAttribute('x', '0')
+	maskBackground.setAttribute('y', '0')
+	maskBackground.setAttribute('width', clientWidth.toString())
+	maskBackground.setAttribute('height', clientHeight.toString())
+	mask.append(maskBackground)
+
+	const maskCutout = document.createElementNS(svgNamespace, 'rect')
+	maskCutout.setAttribute('fill', 'white')
+	mask.append(maskCutout)
+
+	await new Promise(resolve => {
+		svgElement.addEventListener('mousedown', event => {
+			maskCutout.setAttribute('x', event.clientX.toString())
+			maskCutout.setAttribute('y', event.clientY.toString())
+			svgElement.addEventListener('mousemove', event => {
+				maskCutout.setAttribute('width', event.clientX.toString())
+				maskCutout.setAttribute('height', event.clientY.toString())
+			})
+			svgElement.addEventListener('mouseup', resolve)
+		})
+		document.body.append(svgElement)
+	})
+
+	maskCutout.getAttribute('width')
+
+	const svgDocument = documentToSVG(document, {
+		clientBounds: maskCutout.getBoundingClientRect(),
+	})
 
 	console.log('Inlining resources')
 	await inlineResources(svgDocument.documentElement)
