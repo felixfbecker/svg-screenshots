5c2f778515673ea40e56185087ea2d4df892285a
Felix Becker <felix.b@outlook.com>
2020-11-23 00:32:38 +0100

    refactor(inlining): append to document

--- src/background.ts
+++ src/background.ts
@@ -22,6 +22,14 @@ browser.runtime.onMessage.addListener(async (message, sender) => {
 
 async function postProcessSVG(svg: string): Promise<string> {
 	const svgDocument = new DOMParser().parseFromString(svg, 'image/svg+xml')
-	await inlineResources(svgDocument.documentElement)
-	return new XMLSerializer().serializeToString(svgDocument.documentElement)
+	const svgRootElement = (svgDocument.documentElement as Element) as SVGSVGElement
+	// Append to DOM so SVG elements are attached to a window/have defaultView, so window.getComputedStyle() works
+	// This is safe, the generated SVG contains no JavaScript and even if it did, the background page CSP disallows any external or inline scripts.
+	document.body.prepend(svgRootElement)
+	try {
+		await inlineResources(svgRootElement)
+	} finally {
+		svgRootElement.remove()
+	}
+	return new XMLSerializer().serializeToString(svgRootElement)
 }
